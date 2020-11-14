3acbfd37c3a7936e412efc089ef411f64888890f
aymanfarhat <823713+aymanfarhat@users.noreply.github.com>
2020-11-14 12:26:23 +0000

    Deploying to gh-pages from  @ bb743c3763dadabf7cdd0e73491a249054f6217a 🚀

---
+++ precache-manifest.782e9976ec7ba2a23f7835b77dd778da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7f0b783be120f24a32",
+    "url": "/css/app.04897dee.css"
+  },
+  {
+    "revision": "86fc2cce81100ae8924f7b39d3e1769e",
+    "url": "/img/logo.86fc2cce.svg"
+  },
+  {
+    "revision": "6e87aef047be0edacd7f2d23352515df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba7f0b783be120f24a32",
+    "url": "/js/app.6efe9da5.js"
+  },
+  {
+    "revision": "1c7fa4869e35ce0a2b73",
+    "url": "/js/chunk-vendors.5d8dff48.js"
+  },
+  {
+    "revision": "1fee37ce5aba7b27a5ce71028359252d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
